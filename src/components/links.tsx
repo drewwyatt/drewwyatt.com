e77f32509dbef5f0bc357413434c0a60653234a1
Drew Wyatt <drew.j.wyatt@gmail.com>
2020-06-06 11:39:12 -0400

    componentize links

---
+++ src/components/links.tsx
@@ -0,0 +1,63 @@
+import React, { FC } from 'react'
+import EmptySpace from '../components/empty-space'
+import { Github, Twitter } from '../components/icons'
+
+const Links: FC = () => (
+  <>
+    <nav className="nav">
+      <fieldset className="container">
+        <legend className="label">You can also find me at these places</legend>
+        <ul className="links">
+          <li className="link">
+            <a href="https://github.com/drewwyatt" rel="noreferrer" target="_blank">
+              <Github />
+              <EmptySpace nonBreaking />
+              @drewwyatt
+            </a>
+          </li>
+          <li className="link">
+            <a href="https://twitter.com/djwyatt" rel="noreferrer" target="_blank">
+              <Twitter />
+              <EmptySpace nonBreaking />
+              @djwyatt
+            </a>
+          </li>
+        </ul>
+      </fieldset>
+    </nav>
+    <style jsx>{`
+      .nav {
+        margin-top: 20px;
+        width: 100%;
+      }
+
+      .container {
+        border-color: var(--yellow);
+        color: var(--yellow);
+      }
+
+      .label {
+        color: var(--yellow);
+      }
+
+      .links {
+        width: 100%;
+        list-style: none;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-evenly;
+        padding: 0;
+        margin: inherit 0;
+      }
+
+      .link,
+      .link a {
+        display: flex;
+        height: 30px;
+        line-height: 30px;
+        vertical-align: middle;
+      }
+    `}</style>
+  </>
+)
+export default Links
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,34 +1,13 @@
 import React, { FC } from 'react'
 import About from '../components/about'
-import EmptySpace from '../components/empty-space'
-import { Github, Twitter } from '../components/icons'
+import Links from '../components/links'
 import Me from '../components/me'
 
 const Page: FC = () => (
   <div className="container">
     <Me />
     <About />
-    <nav>
-      <fieldset>
-        <legend>You can also find me at these places</legend>
-        <ul className="links">
-          <li className="link">
-            <a href="https://github.com/drewwyatt" rel="noreferrer" target="_blank">
-              <Github />
-              <EmptySpace nonBreaking />
-              @drewwyatt
-            </a>
-          </li>
-          <li className="link">
-            <a href="https://twitter.com/djwyatt" rel="noreferrer" target="_blank">
-              <Twitter />
-              <EmptySpace nonBreaking />
-              @djwyatt
-            </a>
-          </li>
-        </ul>
-      </fieldset>
-    </nav>
+    <Links />
   </div>
 )
 
