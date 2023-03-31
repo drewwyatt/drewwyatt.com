3b12015750639852a9a98426653f66119a452734
Drew Wyatt <drew.j.wyatt@gmail.com>
2023-03-30 21:49:35 -0400

    about about

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -8,8 +8,8 @@ const About: FC = () => (
       <br />
       Right now I mostly make it for
       <EmptySpace />
-      <a target="_blank" rel="noreferrer" href="https://www.onepeloton.com/">
-        Peloton Interactive
+      <a target="_blank" rel="noreferrer" href="https://www.classdojo.com/">
+        ClassDojo
       </a>
       .
     </p>
@@ -18,8 +18,14 @@ const About: FC = () => (
       <EmptySpace />
       <span className="pink">Hooked on Context</span>, you can find the
       <EmptySpace />
-      <a target="_blank" rel="noreferrer" href="https://www.youtube.com/watch?v=8PwAfK_IutI&t=2s">video of that here</a>,
-      and the <a target="_blank" rel="noreferrer" href="https://github.com/drewwyatt/hooked-on-context">sample code here</a>.
+      <a
+        target="_blank"
+        rel="noreferrer"
+        href="https://github.com/drewwyatt/hooked-on-context"
+      >
+        sample code here
+      </a>
+      .
     </p>
   </div>
 )
